3d7fc2762219903c9f00e4f8d18c3e69abc7d946
Danneybleezy <danielolajide2006@gmail.com>
2025-07-15 18:42:22 +0100

    Create bot_modes.py

---
+++ helpers/bot_modes.py
@@ -0,0 +1,7 @@
+from helpers.motivation import run_motivation_mode
+from helpers.crypto_news import run_crypto_news_mode
+from helpers.market_recap import run_market_recap_mode
+from helpers.slang import run_slang_mode
+from helpers.facts import run_facts_mode
+from helpers.quotes import run_quote_mode
+from helpers.fear_greed import run_fear_greed_mode
